9d3fa9a2efc14f582d533db192cd4c6b4b25bc30
Mahesh Kumar <maheshk@squadmindsinc.com>
2019-09-02 18:15:19 +0530

    add the super() and constructor() and this.state{} to the Parent APP class

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import CardList from "./CardList";
 import {Form} from "./Card";
 
 const testData = [
-    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
 ];
 
 class App extends React.Component {
+  constructor(...props){
+    super(...props)
+    this.state={
+      profiles: testData  //It has to be an empty array in the start
+    }
+  }
 
-	render() {
-  	return (
-    	<div>
-    	  <h1 className="header">{this.props.title}</h1>
-        <Form />
-        <CardList profiles={testData}/>
-    	</div>
+  render() {
+    return (
+        <div>
+          <h1 className="header">{this.props.title}</h1>
+          <Form />
+          <CardList profiles={testData}/>
+        </div>
     );
   }
 }
